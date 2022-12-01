b408bfc40711937308ee6a5293d57e0498c26626
C-I_Tom <tom.manahan@hotmail.com>
2022-12-01 19:44:36 +0000

    Add start quiz function with title using ASCII word art

--- run.py
+++ run.py
@@ -12,8 +12,27 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('pub_quiz_challenge')
 
-questions = SHEET.worksheet('questions')
+# questions = SHEET.worksheet('questions')
+# answers = SHEET.worksheet('answers')
 
-data = questions.get_all_values()
+# question_data = questions.get_all_values()
+# answer_data = answers.get_all_values()
 
-print(data[0])
+# print(question_data[1])
+# print(answer_data[1])
+
+def start_quiz():
+    """
+    Start screen with option to start quiz
+    """
+    print("")
+    print(" _______  __   __  _______    _______  __   __  ___   _______    _______  __   __  _______  ___      ___      _______  __    _  _______  _______")
+    print("|       ||  | |  ||  _    |  |       ||  | |  ||   | |       |  |    ___||  | |  ||   _   ||   |    |   |    |       ||  |  | ||       ||       |")
+    print("|    _  ||  | |  || |_|   |  |   _   ||  | |  ||   | |____   |  |   /    |  |_|  ||  |_|  ||   |    |   |    |    ___||   |_| ||    ___||    ___|")
+    print("|   |_| ||  |_|  ||       |  |  | |  ||  |_|  ||   |  ____|  |  |  |     |       ||       ||   |    |   |    |   |___ |       ||   | __ |   |___")
+    print("|    ___||       ||  _   |   |  |_|  ||       ||   | | ______|  |  |     |       ||       ||   |___ |   |___ |    ___||  _    ||   ||  ||    ___|")
+    print("|   |    |       || |_|   |  |      | |       ||   | | |_____   |   \___ |   _   ||   _   ||       ||       ||   |___ | | |   ||   |_| ||   |___ ")
+    print("|___|    |_______||_______|  |____||_||_______||___| |_______|  |_______||__| |__||__| |__||_______||_______||_______||_|  |__||_______||_______|")
+    print("")
+
+start_quiz()
