5ffbd3ce649fc6ecec50f7e6f2e81e3181c51ded
C-I_Tom <tom.manahan@hotmail.com>
2023-01-28 10:51:31 +0000

    Add main function and fix bug that restarts quiz on incorrect entry for topic selection

--- run.py
+++ run.py
@@ -31,177 +31,6 @@ leaderboard = SHEET.worksheet('leaderboard')
 leaderboard_data = leaderboard.get_all_values()
 
 
-def main():
-    """
-    Main function to run the quiz
-    """
-    start_new_quiz()
-
-
-def start_new_quiz():
-    """
-    Allow player to select a topic when quiz starts.
-    Player must enter name and select a topic to begin
-    """
-    answers = []
-    correct_answers = 0
-    num_question = 1
-
-    print("\n--------------------------------------------------------------")
-    print(" _______  __   __  _______    _______  __   __  ___   _______ ")
-    print("|       ||  | |  ||  _    |  |       ||  | |  ||   | |       |")
-    print("|    _  ||  | |  || |_|   |  |   _   ||  | |  ||   | |____   |")
-    print("|   |_| ||  |_|  ||       |  |  | |  ||  |_|  ||   |  ____|  |")
-    print("|    ___||       ||  _   |   |  |_|  ||       ||   | | ______|")
-    print("|   |    |       || |_|   |  |      | |       ||   | | |_____ ")
-    print("|___|    |_______||_______|  |____||_||_______||___| |_______|\n")
-    print("--------------------------------------------------------------")
-    print("")
-
-    player = input("Welcome to Pub Quiz! Enter your name to start: \n")
-    player = player.capitalize()
-
-    print(f"Hi {player}, please choose a topic from the list below: \n")
-
-    print(f"1. {question_data[0][0]}")
-    print(f"2. {question_data[0][1]}")
-    print(f"3. {question_data[0][2]}")
-    print(f"4. {question_data[0][3]}")
-    print(f"5. {question_data[0][4]}\n")
-
-    selection = input("Enter 1, 2, 3, 4 or 5 to start: \n")
-
-    if selection == "1":
-        print("\nYou have selected "+"'"+question_data[0][0]+"', let's begin!")
-
-        for key in science:
-            print("--------------------------------------------------------\n")
-            print(key)
-            for i in science_choices[num_question-1]:
-                print("")
-                print(i)
-
-            choice = input("Enter you answer (A, B or C): \n").upper()
-            answers.append(choice)
-
-            correct_answers += check_correct_answer(science.get(key), choice)
-            num_question += 1
-
-        player_score(correct_answers, answers)
-
-    elif selection == "2":
-        print("\nYou have selected "+"'"+question_data[0][1]+"', let's begin!")
-
-        for key in sports:
-            print("--------------------------------------------------------\n")
-            print(key)
-            for i in sports_choices[num_question-1]:
-                print("")
-                print(i)
-
-            choice = input("Enter you answer (A, B or C): \n").upper()
-            answers.append(choice)
-
-            correct_answers += check_correct_answer(sports.get(key), choice)
-            num_question += 1
-
-        player_score(correct_answers, answers)
-
-    elif selection == "3":
-        print("\nYou have selected "+"'"+question_data[0][2]+"', let's begin!")
-
-        for key in movies_tv:
-            print("--------------------------------------------------------\n")
-            print(key)
-            for i in movies_tv_choices[num_question-1]:
-                print("")
-                print(i)
-
-            choice = input("Enter you answer (A, B or C): \n").upper()
-            answers.append(choice)
-
-            correct_answers += check_correct_answer(movies_tv.get(key), choice)
-            num_question += 1
-
-        player_score(correct_answers, answers)
-
-    elif selection == "4":
-        print("\nYou have selected "+"'"+question_data[0][3]+"', let's begin!")
-
-        for key in music:
-            print("--------------------------------------------------------\n")
-            print(key)
-            for i in music_choices[num_question-1]:
-                print("")
-                print(i)
-
-            choice = input("Enter you answer (A, B or C): \n").upper()
-            answers.append(choice)
-
-            correct_answers += check_correct_answer(music.get(key), choice)
-            num_question += 1
-
-        player_score(correct_answers, answers)
-
-    elif selection == "5":
-        print("\nYou have selected "+"'"+question_data[0][4]+"', let's begin!")
-
-        for key in history:
-            print("--------------------------------------------------------\n")
-            print(key)
-            for i in history_choices[num_question-1]:
-                print("")
-                print(i)
-
-            choice = input("Enter you answer (A, B or C): \n").upper()
-            answers.append(choice)
-
-            correct_answers += check_correct_answer(history.get(key), choice)
-            num_question += 1
-
-        player_score(correct_answers, answers)
-
-    else:
-        print("\nYou entered an incorrect value.Please try again.\n")
-        start_new_quiz()
-
-
-def check_correct_answer(answer, choice):
-    """
-    Checks answer and provides feedback for correct and incorrect answers
-    """
-    if answer == choice:
-        print("You are correct!")
-        return 1
-    else:
-        print()
-        print("Sorry, you are wrong! The correct answer is " + answer)
-        return 0
-
-
-def player_score(correct_answers, answers):
-    """
-    Displays score in percent format by checking number of correct answers
-    against number of questions
-    """
-    print("\n------------------------------------------------------\n")
-
-    score = int((correct_answers/len(answers)) * 100)
-    print("You scored: " + str(score) + "%\n")
-
-
-def play_again():
-    """
-    Allows player to play another game and select a different topic
-    """
-    replay = input("\nWould you like to play again? Enter Y / N: \n").upper()
-
-    if replay == "Y":
-        return True
-    else:
-        return False
-
-
 # Create dictionaries to contain questions and correct answer pairs for topics
 science = {
     question_data[1][0]: "A",
@@ -284,9 +113,188 @@ history_choices = [
     [answer_a_data[5][4], answer_b_data[5][4], answer_c_data[5][4]]
 ]
 
+
+def select_topic():
+    """
+    Allow user to select topic from options provided
+    """
+    answers = []
+    correct_answers = 0
+    num_question = 1
+
+    print(f"1. {question_data[0][0]}")
+    print(f"2. {question_data[0][1]}")
+    print(f"3. {question_data[0][2]}")
+    print(f"4. {question_data[0][3]}")
+    print(f"5. {question_data[0][4]}\n")
+
+    selection = input("Enter 1, 2, 3, 4 or 5 to start: \n")
+
+    if selection == "1":
+        print("\nYou have selected "+"'"+question_data[0][0]+"', let's begin!")
+
+        for key in science:
+            print("--------------------------------------------------------\n")
+            print(key)
+            for i in science_choices[num_question-1]:
+                print("")
+                print(i)
+
+            choice = input("Enter you answer (A, B or C): \n").upper()
+            answers.append(choice)
+
+            correct_answers += check_answer(science.get(key), choice)
+            num_question += 1
+
+        player_score(correct_answers, answers)
+
+    elif selection == "2":
+        print("\nYou have selected "+"'"+question_data[0][1]+"', let's begin!")
+
+        for key in sports:
+            print("--------------------------------------------------------\n")
+            print(key)
+            for i in sports_choices[num_question-1]:
+                print("")
+                print(i)
+
+            choice = input("Enter you answer (A, B or C): \n").upper()
+            answers.append(choice)
+
+            correct_answers += check_answer(sports.get(key), choice)
+            num_question += 1
+
+        player_score(correct_answers, answers)
+
+    elif selection == "3":
+        print("\nYou have selected "+"'"+question_data[0][2]+"', let's begin!")
+
+        for key in movies_tv:
+            print("--------------------------------------------------------\n")
+            print(key)
+            for i in movies_tv_choices[num_question-1]:
+                print("")
+                print(i)
+
+            choice = input("Enter you answer (A, B or C): \n").upper()
+            answers.append(choice)
+
+            correct_answers += check_answer(movies_tv.get(key), choice)
+            num_question += 1
+
+        player_score(correct_answers, answers)
+
+    elif selection == "4":
+        print("\nYou have selected "+"'"+question_data[0][3]+"', let's begin!")
+
+        for key in music:
+            print("--------------------------------------------------------\n")
+            print(key)
+            for i in music_choices[num_question-1]:
+                print("")
+                print(i)
+
+            choice = input("Enter you answer (A, B or C): \n").upper()
+            answers.append(choice)
+
+            correct_answers += check_answer(music.get(key), choice)
+            num_question += 1
+
+        player_score(correct_answers, answers)
+
+    elif selection == "5":
+        print("\nYou have selected "+"'"+question_data[0][4]+"', let's begin!")
+
+        for key in history:
+            print("--------------------------------------------------------\n")
+            print(key)
+            for i in history_choices[num_question-1]:
+                print("")
+                print(i)
+
+            choice = input("Enter you answer (A, B or C): \n").upper()
+            answers.append(choice)
+
+            correct_answers += check_answer(history.get(key), choice)
+            num_question += 1
+
+        player_score(correct_answers, answers)
+
+    else:
+        print("\nYou entered an incorrect value.Please try again.\n")
+        select_topic()
+
+
+def start_new_quiz():
+    """
+    Player must enter their first name and select a topic to begin
+    """
+
+    print("\n--------------------------------------------------------------")
+    print(" _______  __   __  _______    _______  __   __  ___   _______ ")
+    print("|       ||  | |  ||  _    |  |       ||  | |  ||   | |       |")
+    print("|    _  ||  | |  || |_|   |  |   _   ||  | |  ||   | |____   |")
+    print("|   |_| ||  |_|  ||       |  |  | |  ||  |_|  ||   |  ____|  |")
+    print("|    ___||       ||  _   |   |  |_|  ||       ||   | | ______|")
+    print("|   |    |       || |_|   |  |      | |       ||   | | |_____ ")
+    print("|___|    |_______||_______|  |____||_||_______||___| |_______|\n")
+    print("--------------------------------------------------------------")
+    print("")
+
+    player = input("Welcome to Pub Quiz! Enter your first name to start: \n")
+    player = player.capitalize()
+
+    print(f"Hi {player}, please choose a topic from the list below: \n")
+
+    select_topic()
+
+
+def check_answer(answer, choice):
+    """
+    Checks answer and provides feedback for correct and incorrect answers
+    """
+    if answer == choice:
+        print("You are correct!")
+        return 1
+    else:
+        print()
+        print("Sorry, you are wrong! The correct answer is " + answer)
+        return 0
+
+
+def player_score(correct_answers, answers):
+    """
+    Displays score in percent format by checking number of correct answers
+    against number of questions
+    """
+    print("\n------------------------------------------------------\n")
+
+    score = int((correct_answers/len(answers)) * 100)
+    print("You scored: " + str(score) + "%\n")
+
+
+def play_again():
+    """
+    Allows player to play another game and select a different topic
+    """
+    replay = input("\nWould you like to play again? Enter Y / N: \n").upper()
+
+    if replay == "Y":
+        return True
+    else:
+        return False
+
+
+def main():
+    """
+    Main function to run quiz
+    """
+    start_new_quiz()
+
+    while play_again():
+        start_new_quiz()
+
+    print("\nOK Goodbye. Thanks for Playing!\n")
+
+
 main()
-
-while play_again():
-    main()
-
-print("\nOK Goodbye. Thanks for Playing!\n")
